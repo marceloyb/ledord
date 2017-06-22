3ed8a07ac32e12f4f6b8b54161a6709f35daaf91
marceloyb <marcelobenesciutti@gmail.com>
2017-06-22 13:20:58 -0300

    ad

--- main.c
+++ main.c
@@ -2,9 +2,39 @@
 #include <stdlib.h>
 
 
+void menu (){
+  printf ("\n >>>>>>> MENU <<<<<<<\n");
+  printf ("\n (1) para importar\n");
+  printf ("\n (2) para buscar registro pelo numero de inscricao\n");
+  printf ("\n (3) para inserir novo registro\n");
+  printf ("\n (4) para remover registro\n");
+  printf ("\n (0) para fechar o programa\n");
+}
 
 
 int main (){
-
+  int rodando = 1;
+  int opcao;
   FILE *file;
+  do{
+    menu();
+    scanf ("%i", &opcao);
+    switch(opcao){
+      case 1:
+        //import
+        break;
+      case 2:
+        //buscar registro
+        break;
+      case 3:
+        //inserir registro
+        break;
+      case 4:
+        //remover registro
+        break;
+      case 0:
+        rodando = 0;
+        break;
+    }
+  }while(rodando);
 }
